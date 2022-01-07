920f43337b4b97cf570758b9498668f7f80746b2
namx-holi <chucktesta@live.com.au>
2022-01-07 23:38:37 +1100

    Added tmux scripts

---
+++ resume-tmux.sh
@@ -0,0 +1,4 @@
+echo "Connecting to existing tmux session."
+echo "Use CTRL+B the D to detach from the session."
+read
+tmux attach -t 0
---
+++ start-tmux.sh
@@ -0,0 +1,5 @@
+echo "Starting a new tmux session."
+echo "Use CTRL+B the D to detach from the session."
+echo "Use resume-tmux.sh to reconnect to tmux session."
+read
+tmux
